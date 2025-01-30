173d13ea697aea13bf3f76d73a770baa1e2d9f73
NilsMac <nils.henningsen@freenet.de>
2025-01-30 14:51:28 +0100

    Revert "hm"
    This reverts commit 8a3e07f7ef679df7387ee9be74d7e3223b808c92.

--- Assets/Scripts/NetworkStuff/Player/Health.cs
+++ Assets/Scripts/NetworkStuff/Player/Health.cs
@@ -23,6 +23,16 @@ public class Health : NetworkBehaviour
         _previusHealth = _health; 
     }
 
+    private void Update()
+    {
+        if (_previusHealth != _health)
+        {
+            _previusHealth = _health; 
+            _health = _maxHealth;
+            _healthBar.value = _health / _maxHealth;
+
+        }
+    }
     private void Start()
     {
         _healthBar = FindObjectOfType<PlayerHudTag>().GetComponentInChildren<Scrollbar>(); 
@@ -39,12 +49,7 @@ public class Health : NetworkBehaviour
     {
 
         this._health -= damage;
-        if (Runner.LocalPlayer == playerDamaged)
-        {
-            _health = _maxHealth;
-            _healthBar.value = _health / _maxHealth;
-        }
-        if (_health <= 0) 
+        if(_health <= 0) 
         {
             StaticRpcHolder.Rpc_ShowKillFeed(Runner, killer, playerDamaged);
             _cc.enabled = false;
@@ -57,7 +62,5 @@ public class Health : NetworkBehaviour
     public void Rpc_HealUp()
     {
         _health = _maxHealth;
-        _health = _maxHealth;
-        _healthBar.value = _health / _maxHealth;
     }
 }
