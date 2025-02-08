898b87bce59fbe27b64b9955bb300a478145f34c
nilsh <nils.henningsen@freenet.de>
2025-02-08 16:08:20 +0100

    hitbox set scale and offset when crouching

--- Assets/Scripts/NetworkStuff/Player/Crouch.cs
+++ Assets/Scripts/NetworkStuff/Player/Crouch.cs
@@ -7,14 +7,24 @@ using UnityEngine.UIElements;
 
 public class Crouch : NetworkBehaviour 
 {
+    [SerializeField] private Hitbox _hitBox; 
     private Vector3 _originalScale;
+    private Vector3 _hitBoxOSize;
+    private Vector3 _hitBoxCrouchSize;
+    private float _hitBoxOffsetY;
+    private float _hitBoxOffsetYCrouch; 
     private Vector3 _crouchScale;
     private SimpleKCC _cc; 
     public override void Spawned()
     {
         _originalScale = transform.localScale;
-        _crouchScale = _originalScale / 2; 
+        _crouchScale = _originalScale / 2;
+        _hitBoxOSize = _hitBox.BoxExtents;
+        _hitBoxCrouchSize = _hitBoxOSize / 2;
+        _hitBoxOffsetY = _hitBox.Offset.y;
+        _hitBoxOffsetYCrouch = _hitBoxOffsetY / 2f; 
         _cc = this.GetComponent<SimpleKCC>();
+
     }
 
     // Update is called once per frame
@@ -29,27 +39,29 @@ public class Crouch : NetworkBehaviour
             //crouch logic...
             if(data.Buttons.IsSet(MyButtons.Crouch)) 
             {
-                Rpc_SetScale(_crouchScale);
+                Rpc_SetScale(_crouchScale, _hitBoxCrouchSize, _hitBoxOffsetYCrouch);
             } else 
             {
-                Rpc_SetScale(_originalScale);
+                Rpc_SetScale(_originalScale, _hitBoxOSize, _hitBoxOffsetY);
             } 
         }
         else
         {
-            Rpc_SetScale(_originalScale);
+            Rpc_SetScale(_originalScale, _hitBoxOSize, _hitBoxOffsetY);
         }
     }
 
     [Rpc(RpcSources.InputAuthority,RpcTargets.StateAuthority)]
-    public void Rpc_SetScale(Vector3 scale) 
+    public void Rpc_SetScale(Vector3 scale, Vector3 hitBoxSize, float offset) 
     {
-        Rpc_Crouch(scale); 
+        Rpc_Crouch(scale, hitBoxSize, offset); 
     }
 
     [Rpc(RpcSources.StateAuthority, RpcTargets.All)]
-    public void Rpc_Crouch(Vector3 scale) 
+    public void Rpc_Crouch(Vector3 scale, Vector3 hitBoxSize, float offset) 
     {
         transform.localScale = scale;
+        _hitBox.BoxExtents = hitBoxSize;
+        _hitBox.Offset.y = offset; 
     }
 }
